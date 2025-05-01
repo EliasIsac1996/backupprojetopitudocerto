b532853722ad607780a92ed9e8b687f066d90ce5
Elias Isac <115378394+EliasIsac1996@users.noreply.github.com>
2025-05-01 20:57:45 -0300

    backup

---
+++ PI_univesp_2025/README.md
@@ -0,0 +1,119 @@
+# Projeto Integrador Grupo 010 - UNIVESP 2025 (Django com MySQL)
+
+Este projeto utiliza **Django** (qualquer versão 5.x é compatível) e **MySQL** como banco de dados.
+
+
+## ✅ Requisitos
+```
+Antes de iniciar, certifique-se de ter os seguintes pacotes instalados no seu ambiente:
+
+- Python 3.10+ (ou compatível com Django 5)
+- MySQL Server
+- Virtualenv (opcional, mas recomendado)
+
+```
+
+## 📦 Dependências Python
+
+### 1. Criação e ativação do ambiente virtual:
+
+```bash
+python -m venv venv
+source venv/bin/activate     # Linux/macOS
+venv\Scripts\activate        # Windows
+```
+
+### 2. Instalação das dependências:
+
+Instale todas as dependências com:
+
+```bash
+pip install -r requirements.txt
+```
+
+O arquivo `requirements.txt` já contém as bibliotecas necessárias para rodar o projeto:
+
+- **Django 5.2** ou superior
+- **mysqlclient** para conexão com o MySQL
+
+---
+
+## 🗄️ Configuração do Banco de Dados
+
+Este projeto utiliza um banco de dados **MySQL**. Para configurá-lo corretamente, siga os passos abaixo:
+
+### 1. Criar o banco de dados:
+
+Acesse o MySQL e crie o banco de dados:
+
+```sql
+CREATE DATABASE pi_univesp;
+```
+
+### 2. Criar o usuário no MySQL:
+
+Crie um usuário com a senha usada no `settings.py`:
+
+```sql
+CREATE USER 'pi'@'localhost' IDENTIFIED BY '1234';
+GRANT ALL PRIVILEGES ON pi_univesp.* TO 'pi'@'localhost';
+FLUSH PRIVILEGES;
+```
+
+### 3. Migrar o Banco de dados:
+Faça as migrações do banco de dados padrão do Django para o MySQL:
+
+```bash
+python manage.py makemigrations
+python manage.py migrate
+```
+
+### 4. Importar o backup:
+
+Navegue até o diretório fora da pasta do projeto, onde está o arquivo `bkp.sql`, e importe o banco:
+
+```bash
+mysql -u pi -p pi_univesp < bkp.sql
+```
+
+Quando pedir a senha, digite: `1234`
+(Observação: Em caso de atualização, caso ocorra um erro de importação devido a tabelas já criadas, remova o banco anterior e importe a nova versão.
+
+```sql
+DROP DATABASE pi_univesp;
+CREATE DATABASE pi_univesp;
+EXIT;
+```
+```bash
+mysql -u pi -p pi_univesp < bkp.sql
+```
+---
+
+## 🚀 Rodando o Projeto
+
+Depois de configurar o banco de dados e as dependências, você pode rodar o servidor de desenvolvimento do Django com o seguinte comando:
+
+```bash
+python manage.py runserver
+```
+
+Acesse o navegador em:
+
+[http://127.0.0.1:8000/](http://127.0.0.1:8000/)
+
+---
+
+## 🔑 Informações Adicionais
+
+- O login de usuários utiliza a autenticação padrão do Django.
+- Para acessar a área de administração (`/admin`), crie um superusuário (opcional):
+
+```bash
+python manage.py createsuperuser
+```
+
+---
+
+## ⚠️ Observações
+- Certifique-se de que o banco de dados MySQL está em funcionamento antes de rodar o projeto.
+- O arquivo `bkp.sql` deve estar no diretório correto para ser importado com sucesso.
---
+++ PI_univesp_2025/core/views.py
@@ -0,0 +1,433 @@
+from agenda import urls
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from core.models import agendamento_de_aula,Matricula
+from datetime import time
+
+
+#render tela inicial
+@login_required
+def tela_principal(request):
+    #08am - 09am
+    ag_8h_seg = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=2, # Segunda-feira
+        hora__gte=time(8, 0),
+        hora__lt=time(9, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_8h_ter = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=3, # Terça-feira
+        hora__gte=time(8, 0),
+        hora__lt=time(9, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_8h_qua = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=4, # Quarta-feira
+        hora__gte=time(8, 0),
+        hora__lt=time(9, 0)
+    ).first()
+
+    ag_8h_qui = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=5, # Quinta-feira
+        hora__gte=time(8, 0),
+        hora__lt=time(9, 0)
+    ).first()
+
+    ag_8h_sex = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=6, # Sexta-feira
+        hora__gte=time(8, 0),
+        hora__lt=time(9, 0)
+    ).first()
+
+    ag_8h_sab = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=7, # Sábado-feira
+        hora__gte=time(8, 0),
+        hora__lt=time(9, 0)
+    ).first()
+
+    #09am - 10am
+    ag_9h_seg = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=2, # Segunda-feira
+        hora__gte=time(9, 0),
+        hora__lt=time(10, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_9h_ter = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=3, # Terça-feira
+        hora__gte=time(9, 0),
+        hora__lt=time(10, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_9h_qua = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=4, # Quarta-feira
+        hora__gte=time(9, 0),
+        hora__lt=time(10, 0)
+    ).first()
+
+    ag_9h_qui = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=5, # Quinta-feira
+        hora__gte=time(9, 0),
+        hora__lt=time(10, 0)
+    ).first()
+
+    ag_9h_sex = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=6, # Sexta-feira
+        hora__gte=time(9, 0),
+        hora__lt=time(10, 0)
+    ).first()
+
+    ag_9h_sab = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=7, # Sábado-feira
+        hora__gte=time(9, 0),
+        hora__lt=time(10, 0)
+    ).first()
+
+    #010am - 11am
+    ag_10h_seg = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=2, # Segunda-feira
+        hora__gte=time(9, 0),
+        hora__lt=time(10, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_10h_ter = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=3, # Terça-feira
+        hora__gte=time(9, 0),
+        hora__lt=time(10, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_10h_qua = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=4, # Quarta-feira
+        hora__gte=time(9, 0),
+        hora__lt=time(10, 0)
+    ).first()
+
+    ag_10h_qui = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=5, # Quinta-feira
+        hora__gte=time(9, 0),
+        hora__lt=time(10, 0)
+    ).first()
+
+    ag_10h_sex = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=6, # Sexta-feira
+        hora__gte=time(9, 0),
+        hora__lt=time(10, 0)
+    ).first()
+
+    ag_10h_sab = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=7, # Sábado-feira
+        hora__gte=time(9, 0),
+        hora__lt=time(10, 0)
+    ).first()
+
+    #11am - 12pm
+    ag_11h_seg = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=2, # Segunda-feira
+        hora__gte=time(10, 0),
+        hora__lt=time(11, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_11h_ter = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=3, # Terça-feira
+        hora__gte=time(10, 0),
+        hora__lt=time(11, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_11h_qua = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=4, # Quarta-feira
+        hora__gte=time(10, 0),
+        hora__lt=time(11, 0)
+    ).first()
+
+    ag_11h_qui = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=5, # Quinta-feira
+        hora__gte=time(10, 0),
+        hora__lt=time(11, 0)
+    ).first()
+
+    ag_11h_sex = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=6, # Sexta-feira
+        hora__gte=time(10, 0),
+        hora__lt=time(11, 0)
+    ).first()
+
+    ag_11h_sab = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=7, # Sábado-feira
+        hora__gte=time(10, 0),
+        hora__lt=time(11, 0)
+    ).first()
+
+    #12pm - 13pm
+    ag_12h_seg = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=2, # Segunda-feira
+        hora__gte=time(11, 0),
+        hora__lt=time(12, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_12h_ter = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=3, # Terça-feira
+        hora__gte=time(11, 0),
+        hora__lt=time(12, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_12h_qua = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=4, # Quarta-feira
+        hora__gte=time(11, 0),
+        hora__lt=time(12, 0)
+    ).first()
+
+    ag_12h_qui = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=5, # Quinta-feira
+        hora__gte=time(11, 0),
+        hora__lt=time(12, 0)
+    ).first()
+
+    ag_12h_sex = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=6, # Sexta-feira
+        hora__gte=time(11, 0),
+        hora__lt=time(12, 0)
+    ).first()
+
+    ag_12h_sab = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=7, # Sábado-feira
+        hora__gte=time(11, 0),
+        hora__lt=time(12, 0)
+    ).first()
+
+    #13pm - 14pm
+    ag_13h_seg = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=2, # Segunda-feira
+        hora__gte=time(12, 0),
+        hora__lt=time(13, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_13h_ter = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=3, # Terça-feira
+        hora__gte=time(12, 0),
+        hora__lt=time(13, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_13h_qua = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=4, # Quarta-feira
+        hora__gte=time(12, 0),
+        hora__lt=time(13, 0)
+    ).first()
+
+    ag_13h_qui = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=5, # Quinta-feira
+        hora__gte=time(12, 0),
+        hora__lt=time(13, 0)
+    ).first()
+
+    ag_13h_sex = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=6, # Sexta-feira
+        hora__gte=time(12, 0),
+        hora__lt=time(13, 0)
+    ).first()
+
+    ag_13h_sab = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=7, # Sábado-feira
+        hora__gte=time(12, 0),
+        hora__lt=time(13, 0)
+    ).first()
+
+    #14pm - 15pm
+    ag_14h_seg = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=2, # Segunda-feira
+        hora__gte=time(13, 0),
+        hora__lt=time(14, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_14h_ter = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=3, # Terça-feira
+        hora__gte=time(13, 0),
+        hora__lt=time(14, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_14h_qua = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=4, # Quarta-feira
+        hora__gte=time(13, 0),
+        hora__lt=time(14, 0)
+    ).first()
+
+    ag_14h_qui = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=5, # Quinta-feira
+        hora__gte=time(13, 0),
+        hora__lt=time(14, 0)
+    ).first()
+
+    ag_14h_sex = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=6, # Sexta-feira
+        hora__gte=time(13, 0),
+        hora__lt=time(14, 0)
+    ).first()
+
+    ag_14h_sab = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=7, # Sábado-feira
+        hora__gte=time(13, 0),
+        hora__lt=time(14, 0)
+    ).first()
+
+    #15pm - 16pm
+    ag_15h_seg = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=2, # Segunda-feira
+        hora__gte=time(14, 0),
+        hora__lt=time(15, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_15h_ter = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=3, # Terça-feira
+        hora__gte=time(14, 0),
+        hora__lt=time(15, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_15h_qua = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=4, # Quarta-feira
+        hora__gte=time(14, 0),
+        hora__lt=time(15, 0)
+    ).first()
+
+    ag_15h_qui = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=5, # Quinta-feira
+        hora__gte=time(14, 0),
+        hora__lt=time(15, 0)
+    ).first()
+
+    ag_15h_sex = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=6, # Sexta-feira
+        hora__gte=time(14, 0),
+        hora__lt=time(15, 0)
+    ).first()
+
+    ag_15h_sab = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=7, # Sábado-feira
+        hora__gte=time(14, 0),
+        hora__lt=time(15, 0)
+    ).first()
+
+    #16pm - 17pm
+    ag_16h_seg = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=2, # Segunda-feira
+        hora__gte=time(15, 0),
+        hora__lt=time(16, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_16h_ter = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=3, # Terça-feira
+        hora__gte=time(15, 0),
+        hora__lt=time(16, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_16h_qua = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=4, # Quarta-feira
+        hora__gte=time(15, 0),
+        hora__lt=time(16, 0)
+    ).first()
+
+    ag_16h_qui = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=5, # Quinta-feira
+        hora__gte=time(15, 0),
+        hora__lt=time(16, 0)
+    ).first()
+
+    ag_16h_sex = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=6, # Sexta-feira
+        hora__gte=time(15, 0),
+        hora__lt=time(16, 0)
+    ).first()
+
+    ag_16h_sab = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=7, # Sábado-feira
+        hora__gte=time(15, 0),
+        hora__lt=time(16, 0)
+    ).first()
+    
+    #17pm - 18pm
+    ag_17h_seg = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=2, # Segunda-feira
+        hora__gte=time(16, 0),
+        hora__lt=time(17, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_17h_ter = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=3, # Terça-feira
+        hora__gte=time(16, 0),
+        hora__lt=time(17, 0)
+    ).first()  # Pega apenas o primeiro, se houver
+
+    ag_17h_qua = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=4, # Quarta-feira
+        hora__gte=time(16, 0),
+        hora__lt=time(17, 0)
+    ).first()
+
+    ag_17h_qui = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=5, # Quinta-feira
+        hora__gte=time(16, 0),
+        hora__lt=time(17, 0)
+    ).first()
+
+    ag_17h_sex = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=6, # Sexta-feira
+        hora__gte=time(16, 0),
+        hora__lt=time(17, 0)
+    ).first()
+
+    ag_17h_sab = agendamento_de_aula.objects.filter(
+        data_agendamento__week_day=7, # Sábado-feira
+        hora__gte=time(16, 0),
+        hora__lt=time(17, 0)
+    ).first()
+
+    return render(request, 'tela_principal.html', {
+        'ag_8h_seg': ag_8h_seg, 'ag_8h_ter': ag_8h_ter,
+        'ag_8h_qua': ag_8h_qua, 'ag_8h_qui': ag_8h_qui,
+        'ag_8h_sex': ag_8h_sex, 'ag_8h_sab': ag_8h_sab,
+
+        'ag_9h_seg': ag_9h_seg, 'ag_9h_ter': ag_9h_ter,
+        'ag_9h_qua': ag_9h_qua, 'ag_9h_qui': ag_9h_qui,
+        'ag_9h_sex': ag_9h_sex, 'ag_9h_sab': ag_9h_sab,
+
+        'ag_10h_seg': ag_10h_seg, 'ag_10h_ter': ag_10h_ter,
+        'ag_10h_qua': ag_10h_qua, 'ag_10h_qui': ag_10h_qui,
+        'ag_10h_sex': ag_10h_sex, 'ag_10h_sab': ag_10h_sab,
+
+        'ag_11h_seg': ag_11h_seg, 'ag_11h_ter': ag_11h_ter,
+        'ag_11h_qua': ag_11h_qua, 'ag_11h_qui': ag_11h_qui,
+        'ag_11h_sex': ag_11h_sex, 'ag_11h_sab': ag_11h_sab,
+
+        'ag_12h_seg': ag_12h_seg, 'ag_12h_ter': ag_12h_ter,
+        'ag_12h_qua': ag_12h_qua, 'ag_12h_qui': ag_12h_qui,
+        'ag_12h_sex': ag_12h_sex, 'ag_12h_sab': ag_12h_sab,
+
+        'ag_13h_seg': ag_13h_seg, 'ag_13h_ter': ag_13h_ter,
+        'ag_13h_qua': ag_13h_qua, 'ag_13h_qui': ag_13h_qui,
+        'ag_13h_sex': ag_13h_sex, 'ag_13h_sab': ag_13h_sab,
+
+        'ag_14h_seg': ag_14h_seg, 'ag_14h_ter': ag_14h_ter,
+        'ag_14h_qua': ag_14h_qua, 'ag_14h_qui': ag_14h_qui,
+        'ag_14h_sex': ag_14h_sex, 'ag_14h_sab': ag_14h_sab,
+
+        'ag_15h_seg': ag_15h_seg, 'ag_15h_ter': ag_15h_ter,
+        'ag_15h_qua': ag_15h_qua, 'ag_15h_qui': ag_15h_qui,
+        'ag_15h_sex': ag_15h_sex, 'ag_15h_sab': ag_15h_sab,
+
+        'ag_16h_seg': ag_16h_seg, 'ag_16h_ter': ag_16h_ter,
+        'ag_16h_qua': ag_16h_qua, 'ag_16h_qui': ag_16h_qui,
+        'ag_16h_sex': ag_16h_sex, 'ag_16h_sab': ag_16h_sab,
+
+        'ag_17h_seg': ag_17h_seg, 'ag_17h_ter': ag_17h_ter,
+        'ag_17h_qua': ag_17h_qua, 'ag_17h_qui': ag_17h_qui,
+        'ag_17h_sex': ag_17h_sex, 'ag_17h_sab': ag_17h_sab, 
+    })
+
+def matriculasubmit(request) :
+    if request.POST:
+      nome_completo = request.POST.get('nome_completo')
+      idade  = request.POST.get('idade')
+      instrumento = request.POST.get('instrumento')
+      email = request.POST.get('email')
+      telefone= request.POST.get('telefone')
+      observacao =  request.POST.get('observacoes')
+      instrutor = request.user
+      Matricula.objects.create(nome_completo= nome_completo ,idade = idade ,instrumento = instrumento, email = email, telefone = telefone,observacao = observacao, instrutor = instrutor)
+    return redirect('tela_principal')
